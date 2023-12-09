86cc66bdce463982ee6aaaad575b41e69426ff6c
freesiat <freesiatokunaga2025@u.northwestern.edu>
2023-12-08 19:42:10 -0600

    commit correct data files

--- 0a_data_collection.R
+++ 0a_data_collection.R
@@ -1,25 +1,29 @@
 ## Data quality & complexity check
 
+#load packages
+library(tidyverse)
+library(skimr)
+library(readxl)
+library(raster)
 
 #all healthcare sites in Japan
-japan_healthsites <- read_csv("data/japan.csv") |> 
+japan_healthsites <- read_csv("data/raw_data/japan.csv") |> 
   dplyr::select(X, Y, amenity)
 
-#number of citizens over 65 years old in every city in Japan
-geriatric_population <- read_excel("data/geriatric_city.xlsx")
+#percentage of citizens over 65 years old in every city in Japan
+geriatric_population <- read_excel("data/raw_data/geriatric_city.xlsx")
 janitor::clean_names(geriatric_population)  
-
-#drop "city" from city name 
-gp_new <- as.data.frame(geriatric_population) |> 
-  mutate(city_name = word(city, 1)) |> 
-  dplyr::select(year_2020_percentage, city_name) 
 
 #dataframe of japan with all cities
 jpn <- getData("GADM", country = "JPN", level = 2)
 jpn_df <- sf::st_as_sf(jpn, region = "NAME_2")
 janitor::clean_names(jpn_df) 
 
+#number of citizens over 65 years old in each of Japan's prefectures
+prefecture_gp <- read_xlsx("data/raw_data/prefecture_population.xlsx") 
+janitor::clean_names(prefecture_gp)
+
 #overview of variable types and missingness
 skimr::skim_without_charts(japan_healthsites)
-skimr::skim_without_charts(gp_new)
+skimr::skim_without_charts(geriatric_population)
 skimr::skim_without_charts(jpn_df)
